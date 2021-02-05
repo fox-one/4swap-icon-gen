337994e1bd5e5ca7b818ce293d8b1f34b63a5752
Lyric <5h3ll3x@gmail.com>
2021-02-05 16:49:26 +0900

    feat: generate pr required files; get images from mixin network

--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module github.com/fox-one/4swap-icon-gen
 go 1.15
 
 require (
+	github.com/cenkalti/dominantcolor v0.0.0-20171020061837-df772e8dd39e
+	github.com/fox-one/mixin-sdk-go v1.2.12
+	github.com/marekm4/color-extractor v1.2.0
 	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
 	github.com/srwiley/oksvg v0.0.0-20200311192757-870daf9aa564
 	github.com/srwiley/rasterx v0.0.0-20200120212402-85cb7272f5e9
--- main.go
+++ main.go
@@ -2,34 +2,76 @@ package main
 
 import (
 	"bytes"
+	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"image"
+	"image/color"
 	"image/draw"
 	"image/png"
+	"io"
 	"io/ioutil"
+	"log"
+	"net/http"
 	"os"
+	"path"
 	"strings"
 
+	"github.com/fox-one/mixin-sdk-go"
+	color_extractor "github.com/marekm4/color-extractor"
 	"github.com/nfnt/resize"
 	"github.com/srwiley/oksvg"
 	"github.com/srwiley/rasterx"
 )
 
-// usage
-// 	./4swap-icon-gen -c1 "#F7931B" -c2 "#000000" -ic1 ./btc.png -ic2 ./dot.png -t sBTC-DOT
+/*
+usages
+	- default
+ 	./4swap-icon-gen -config keystore.json -o NEW_ASSET_ID -a1 54c61a72-b982-4034-a556-0d99e3c21e39 -a2 a31e847e-ca87-3162-b4d1-322bc552e831
 
-var color1 = flag.String("c1", "#FF0000", "the first color")
-var color2 = flag.String("c2", "#0000FF", "the second color")
-var icon1 = flag.String("ic1", "icon1.png", "the first icon")
-var icon2 = flag.String("ic2", "icon2.png", "the second icon")
-var tokenName = flag.String("t", "sAAA-BBB", "the lp token name")
+ 	- override icons
+	./4swap-icon-gen -config keystore.json -o NEW_ASSET_ID -a1 54c61a72-b982-4034-a556-0d99e3c21e39 -a2 a31e847e-ca87-3162-b4d1-322bc552e831 -ic1 ./btc.png -ic2 ./dot.png
+
+  - override colors
+	./4swap-icon-gen -config keystore.json -o NEW_ASSET_ID -a1 54c61a72-b982-4034-a556-0d99e3c21e39 -a2 a31e847e-ca87-3162-b4d1-322bc552e831 -c1 "#F7931B" -c2 "#000000"
+*/
+
+var (
+	config = flag.String("config", "", "keystore file path")
+	pin    = flag.String("pin", "", "pin")
+
+	ctx = context.Background()
+
+	outputPath = flag.String("o", "", "the output path")
+	newAssetID = flag.String("a0", "", "the output asset id")
+	assetID1   = flag.String("a1", "", "the first asset id")
+	assetID2   = flag.String("a2", "", "the second asset id")
+	color1     = flag.String("c1", "", "the first color (in hex)")
+	color2     = flag.String("c2", "", "the second color (in hex)")
+	icon1      = flag.String("ic1", "", "the first icon")
+	icon2      = flag.String("ic2", "", "the second icon")
+)
 
 const (
-	SIZE      = 520
-	ICON_SIZE = 240
-	GAP       = 40
+	SIZE        = 520
+	ICON_SIZE   = 240
+	GAP         = 40
+	OUTPUT_PATH = "output"
 )
+
+func hexColor(c color.Color) string {
+	rgba := color.RGBAModel.Convert(c).(color.RGBA)
+	return fmt.Sprintf("#%.2x%.2x%.2x", rgba.R, rgba.G, rgba.B)
+}
+
+func getPath() string {
+	base := OUTPUT_PATH
+	if *outputPath != "" {
+		base = *outputPath
+	}
+	return path.Join(base, *newAssetID)
+}
 
 func loadSVG(c1, c2 string) []byte {
 	data, err := ioutil.ReadFile("tpl.svg")
@@ -37,36 +79,70 @@ func loadSVG(c1, c2 string) []byte {
 		panic(err)
 	}
 	s := string(data)
+
+	if *color1 != "" {
+		c1 = *color1
+	}
+	if *color2 != "" {
+		c2 = *color2
+	}
 	s = strings.ReplaceAll(s, "#DE400B", c1)
 	s = strings.ReplaceAll(s, "#19C0FC", c2)
 	return []byte(s)
 }
 
-func loadIcons() (image.Image, image.Image) {
-	imgFile1, err := os.Open(*icon1)
+func loadIcons(url1, url2 string) (img1, img2 image.Image, c1, c2 string, err error) {
+	resp1, err := http.Get(url1)
 	if err != nil {
-		fmt.Println(err)
+		return
 	}
-	imgFile2, err := os.Open(*icon2)
+	defer resp1.Body.Close()
+
+	resp2, err := http.Get(url2)
 	if err != nil {
-		fmt.Println(err)
+		return
+	}
+	defer resp1.Body.Close()
+
+	var imgFile1, imgFile2 io.Reader
+	if *icon1 != "" {
+		imgFile1, err = os.Open(*icon1)
+		if err != nil {
+			return
+		}
+	} else {
+		imgFile1 = resp1.Body
 	}
 
-	img1, _, err := image.Decode(imgFile1)
-	if err != nil {
-		fmt.Println(err)
+	if *icon2 != "" {
+		imgFile2, err = os.Open(*icon2)
+		if err != nil {
+			return
+		}
+	} else {
+		imgFile2 = resp2.Body
 	}
-	img2, _, err := image.Decode(imgFile2)
+
+	img1, _, err = image.Decode(imgFile1)
 	if err != nil {
-		fmt.Println(err)
+		return
+	}
+	img2, _, err = image.Decode(imgFile2)
+	if err != nil {
+		return
 	}
 	img1 = resize.Resize(ICON_SIZE, ICON_SIZE, img1, resize.Lanczos3)
 	img2 = resize.Resize(ICON_SIZE, ICON_SIZE, img2, resize.Lanczos3)
 
-	return img1, img2
+	colors1 := color_extractor.ExtractColors(img1)
+	colors2 := color_extractor.ExtractColors(img2)
+
+	c1 = hexColor(colors1[0])
+	c2 = hexColor(colors2[0])
+	return
 }
 
-func genPNG(svg []byte, img1, img2 image.Image) {
+func genPNG(symbol string, svg []byte, img1, img2 image.Image) {
 
 	// load from svg tpl
 	icon, _ := oksvg.ReadIconStream(bytes.NewReader(svg))
@@ -84,7 +160,8 @@ func genPNG(svg []byte, img1, img2 image.Image) {
 	draw.Draw(rgba, r2, img2, image.Point{0, 0}, draw.Over)
 
 	// write png
-	out, err := os.Create(fmt.Sprintf("%s.png", *tokenName))
+	assetPath := getPath()
+	out, err := os.Create(path.Join(assetPath, fmt.Sprintf("%s.png", symbol)))
 	if err != nil {
 		panic(err)
 	}
@@ -96,9 +173,88 @@ func genPNG(svg []byte, img1, img2 image.Image) {
 	}
 }
 
+func loadConfig() *mixin.Client {
+	f, err := os.Open(*config)
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	var store mixin.Keystore
+	if err := json.NewDecoder(f).Decode(&store); err != nil {
+		log.Panicln(err)
+	}
+
+	client, err := mixin.NewFromKeystore(&store)
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	return client
+}
+
+func getAssets(client *mixin.Client) (asset1 *mixin.Asset, asset2 *mixin.Asset, err error) {
+	asset1, err = client.ReadAsset(ctx, *assetID1)
+	if err != nil {
+		return
+	}
+	asset2, err = client.ReadAsset(ctx, *assetID2)
+	if err != nil {
+		return
+	}
+	return
+}
+
+func genName(sym1, sym2 string) string {
+	if sym1 == "pUSD" {
+		return fmt.Sprintf("s%s-%s", strings.ToUpper(sym2), sym1)
+	} else if sym2 == "pUSD" {
+		return fmt.Sprintf("s%s-%s", strings.ToUpper(sym1), sym2)
+	}
+	return fmt.Sprintf("s%s-%s", strings.ToUpper(sym1), strings.ToUpper(sym2))
+}
+
+func genJSON() (err error) {
+	tpl := `{
+	"asset_id":"%s",
+	"chain_id": "43d61dcd-e413-450d-80b8-101d5e903357",
+	"cmc_id": ""
+}
+`
+	assetPath := getPath()
+	os.MkdirAll(assetPath, os.ModePerm)
+	data := fmt.Sprintf(tpl, *newAssetID)
+	err = ioutil.WriteFile(path.Join(assetPath, "index.json"), []byte(data), os.ModePerm)
+	return
+}
+
 func main() {
 	flag.Parse()
-	base := loadSVG(*color1, *color2)
-	img1, img2 := loadIcons()
-	genPNG(base, img1, img2)
+	// load config
+	client := loadConfig()
+
+	// get two assets from mixin
+	asset1, asset2, err := getAssets(client)
+	if err != nil {
+		log.Panicln(err)
+		return
+	}
+
+	// fetch icons
+	img1, img2, c1, c2, err := loadIcons(asset1.IconURL, asset2.IconURL)
+	if err != nil {
+		log.Panicln(err)
+		return
+	}
+
+	// load template
+	tpl := loadSVG(c1, c2)
+
+	// generate token name
+	symbol := genName(asset1.Symbol, asset2.Symbol)
+
+	// gen json files
+	genJSON()
+
+	// gen png
+	genPNG(symbol, tpl, img1, img2)
 }
